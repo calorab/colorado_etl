82108db3aa1eaf8d029726ef7ca84a78754d0b01
Calorab <calebwgammon@gmail.com>
2023-09-13 17:16:50 -0400

    first commit - folder organization (basic), SQL account setup and comments for API file

---
+++ 01_SetupSnowflake.sql
@@ -0,0 +1,50 @@
+-- ----------------------------------------------------------------------------
+-- Step #1: Accept Anaconda Terms & Conditions
+-- ----------------------------------------------------------------------------
+
+-- See Getting Started section in Third-Party Packages (https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-packages.html#getting-started)
+
+
+-- ----------------------------------------------------------------------------
+-- Step #2: Create the account level objects
+-- ----------------------------------------------------------------------------
+-- SELECT THE ROLE THAT WE WILL USE FOR ALL FOLLOWING QUERIES - MUST BE ACCOUNTADMIN SINCE WE ARE CREATING SYSADMIN ROLE(S)
+USE ROLE ACCOUNTADMIN;
+
+-- Roles
+-- SET THE VARIABLE MY_USER TO THE DEFAULT OR CURRENT SYSTEM USER 
+SET MY_USER = CURRENT_USER();
+
+-- CREATE THE MAIN ROLE, GIVE IT SYSADMIN PRIVILEGES AND GRANT THAT ROLE TO MY_USER
+CREATE OR REPLACE ROLE COL_ADMIN;
+GRANT ROLE COL_ADMIN TO ROLE SYSADMIN;
+GRANT ROLE COLE_ADMIN TO USER IDENTIFIER($MY_USER);
+
+-- GIVE EXECUTE TASK AND MONITOR EXECUTION PRIVILEGES TO THE MAIN ROLE (FIND OUT WHAT IMPORTED PRIVILEGES ARE)
+GRANT EXECUTE TASK ON ACCOUNT TO ROLE COL_ADMIN;
+GRANT MONITOR EXECUTION ON ACCOUNT TO ROLE COL_ADMIN;
+GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE TO ROLE COL_ADMIN;
+
+-- Databases CREATE THE DATABASE(S) AND GRANT THE OWNERSHIP TO THE MAIN ROLE SINCE WE ARE IN ACCOUNTADMIN ROLE
+CREATE OR REPLACE DATABASE COLORADO;
+GRANT OWNERSHIP ON DATABASE COLORADO TO ROLE COL_ADMIN;
+
+-- Warehouses
+-- CREATE THE WAREHOUSE(S) AND GRANT THE OWNERSHIP TO THE MAIN ROLE SINCE WE ARE IN ACCOUNTADMIN ROLE
+CREATE OR REPLACE WAREHOUSE COL_WH;
+GRANT OWNERSHIP ON WAREHOUSE COL_WH TO ROLE COL_ADMIN;
+
+-- ----------------------------------------------------------------------------
+-- Step #3: Create the database level objects
+-- ----------------------------------------------------------------------------
+USE ROLE COL_ADMIN;
+USE DATABASE COLORADO;
+USE WAREHOUSE COL_WH;
+
+-- Schemas
+
+CREATE OR REPLACE SCHEMA ANALYTICS;
+CREATE OR REPLACE SCHEMA WEBSCRAPE;
+CREATE OR REPLACE SCHEMA EXTERNAL;
+
+
---
+++ 02_Load_Raw_API_Data.py
@@ -0,0 +1,23 @@
+#  first line
+
+# need snowpark, pandas,
+# Keep the URL's here - maybe in a tuple and loop over it
+
+
+#  Main function
+def main(url):
+    # first list the variables needed and open Snowpark session
+    
+    # make the API call on "url" - include testing for err's in try/catch/exception block
+
+    # assign the returned data to var
+
+    #   if data type JSON - upload to raw table (or whatever SF recommends - UPDATE HERE WHEN YOU KNOW)
+    #       Need to use Snowpark for session object, and writing sql with python
+    #   if data type CSV use pandas to upload to table
+
+    #  close session
+    
+    pass
+
+
