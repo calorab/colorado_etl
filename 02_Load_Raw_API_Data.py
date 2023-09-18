4d3db850adf59ccb89a49c5f512ea800637ed80a
Calorab <calebwgammon@gmail.com>
2023-09-18 16:58:55 -0400

    tested snowpark session creation successfully

--- 02_Load_Raw_API_Data.py
+++ 02_Load_Raw_API_Data.py
@@ -1,13 +1,23 @@
 #  first line
 
 # need snowpark, pandas,
+from snowflake.snowpark import Session
 # Keep the URL's here - maybe in a tuple and loop over it
 
 
 #  Main function
 def main():
     # first list any variables needed and open Snowpark session
+    conn_params: dict = {
+        "account": "omb53008.us-east-1",
+        "user": "ETLPROG2023",
+        "password": "NHqLFb2X6#CgoLtn"
+    }
+#  https://omb53008.us-east-1.snowflakecomputing.com
     
+    new_session = Session.builder.configs(conn_params).create()
+    #  some code here
+    new_session.close()
     # make the API call on "url" - include testing for err's in try/catch/exception block
 
     # assign the returned data to var
@@ -17,9 +27,10 @@ def main():
     #   if data type CSV use pandas to upload to table
 
     #  close session
-    
-    pass
 
 
 def get_parks_rec(url, session):
     pass
+
+
+main()
