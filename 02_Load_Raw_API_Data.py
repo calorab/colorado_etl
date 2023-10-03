3d9bcf4a00ceb9bc24a5c67eee58d2164bee2c38
Calorab <calebwgammon@gmail.com>
2023-10-03 14:08:25 -0400

    added POI function to get and upload basic data

--- 02_Load_Raw_API_Data.py
+++ 02_Load_Raw_API_Data.py
@@ -29,45 +29,50 @@ def main():
     cur.execute('USE SCHEMA EXTERNAL;')
     cur.execute('USE WAREHOUSE COL_WH;')
 
-    # Get a list of all the files in the JSON_Docs folder
-    folder_path = 'JSON_Docs'
-    file_list = os.listdir(folder_path)
+    # Call POI API endpoint, build DB's and insert rows
+    get_poi_data(conn)
 
-    # Loop through the list of files and perform an action on each file
-    for file_name in file_list:
-        unique_file_name = os.path.join('file:///Users/AllHeart/Desktop/Projects_2023/coloradoproject_snowflake/JSON_Docs', file_name)
-        db_addition = os.path.splitext(file_name)[0]
+    # # Get a list of all the files in the JSON_Docs folder
+    # folder_path = 'JSON_Docs'
+    # file_list = os.listdir(folder_path)
 
-        # Stage the parks JSON data
-        try:
-            cur.execute(f'PUT {unique_file_name} @api_stage AUTO_COMPRESS=TRUE;')
-        except snowflake.connector.errors.ProgrammingError as e:
-            print(f'\t {e}')
+    # # Loop through the list of files and perform an action on each file
+    # for file_name in file_list:
+    #     unique_file_name = os.path.join('file:///Users/AllHeart/Desktop/Projects_2023/coloradoproject_snowflake/JSON_Docs', file_name)
+    #     db_addition = os.path.splitext(file_name)[0]
+
+    #     # Stage the parks JSON data
+    #     try:
+    #         cur.execute(f'PUT {unique_file_name} @api_stage AUTO_COMPRESS=TRUE;')
+    #     except snowflake.connector.errors.ProgrammingError as e:
+    #         print(f'\t {e}')
 
 
-        #  create raw JSON formatted data table
-        try:
-            unique_raw_name = 'raw_comm_' + db_addition
-            raw_script = f"CREATE OR REPLACE TABLE {unique_raw_name} AS SELECT $1 FROM @api_stage (FILE_FORMAT => 'api_json_format');"
-            cur.execute(raw_script)
-        except snowflake.connector.errors.ProgrammingError as e:
-            print(f'\t {e}')
-        else:
-            # clear the stage of all files
-            cur.execute("REMOVE @api_stage")    
+    #     #  create raw JSON formatted data table
+    #     try:
+    #         unique_raw_name = 'raw_comm_' + db_addition
+    #         raw_script = f"CREATE OR REPLACE TABLE {unique_raw_name} AS SELECT $1 FROM @api_stage (FILE_FORMAT => 'api_json_format');"
+    #         cur.execute(raw_script)
+    #     except snowflake.connector.errors.ProgrammingError as e:
+    #         print(f'\t {e}')
+    #     else:
+    #         # clear the stage of all files
+    #         cur.execute("REMOVE @api_stage")    
         
 
-        #  create flattened JSON formatted data table for files that need it
-        if file_name in ('parksrec.json'):
-            try:
-                unique_flat_name = 'flat_comm_' + db_addition
-                # CALEB - need to make a change here to get flattened
-                flattened_script = f'CREATE OR REPLACE TABLE {unique_flat_name} as SELECT VALUE FROM {unique_raw_name}, LATERAL FLATTEN(INPUT => SRC:data)'
-                cur.execute(flattened_script)
-            except snowflake.connector.errors.ProgrammingError as e:
-                print(f'\t {e}')
-                        
+    #     #  create flattened JSON formatted data table for files that need it
+    #     if file_name in ('parksrec.json'):
+    #         try:
+    #             unique_flat_name = 'flat_comm_' + db_addition
+    #             # CALEB - need to make a change here to get flattened
+    #             flattened_script = f'CREATE OR REPLACE TABLE {unique_flat_name} as SELECT VALUE FROM {unique_raw_name}, LATERAL FLATTEN(INPUT => SRC:data)'
+    #             cur.execute(flattened_script)
+    #         except snowflake.connector.errors.ProgrammingError as e:
+    #             print(f'\t {e}')
     
+    # # CALEB - Need to clean up json files after they are loaded
+
+
     # end the Snowflake session
     conn.close()
 
@@ -87,11 +92,47 @@ def get_parks_rec():
             json.dump(results, f, indent=4)
 
     
-def get_poi_data():
+def get_poi_data(conn):
     #  Geoapify API for points of interest base url: https://api.geoapify.com/v2/places?PARAMS
     api_key = 'fca84109c53b4439aa9e4c2ca1348525'
-    pass
+    url_map = {'jefferson': '5170cd374708505ac0596bf2479314cb4340f00101f901ff88150000000000c002099203104a6566666572736f6e20436f756e7479',
+               'denver': '513795b35c0a385ac059fc7fdca68de14340f00101f9010b89150000000000c0020992030644656e766572',
+               'boulder': '5114becfbee3565ac0597ff9b363da0b4440f00101f9010989150000000000c0020992030e426f756c64657220436f756e7479',
+               'douglas': '51aa9a54167e3b5ac05959e5f0e533aa4340f00101f9010a89150000000000c0020992030e446f75676c617320436f756e7479',
+               'adams': '51a148d03d9e155ac059ea97b4acd3ef4340f00101f9011289150000000000c0020992030c4164616d7320436f756e7479',
+               'arapahoe': '51def06ba5b7155ac059a7b5c85f27d34340f00101f9011789150000000000c0020992030f4172617061686f6520436f756e7479',
+               'larimer': '51ca1d3fe4815d5ac05930cf56d952554440f00101f901fb88150000000000c0020992030e4c6172696d657220436f756e7479',
+               'elpaso': '517fd481c5a1215ac059a907cc27836a4340f00101f9010789150000000000c0020992030e456c205061736f20436f756e7479'
+               }
+    poi = (
+        'commercial.pet',
+        'pet.veterinary',
+        'pet.dog_park',
+        'leisure.park',
+        'populated_place.neighbourhood',
+        'natural.mountain,natural.forest',
+        'catering.restaurant.sushi'
+    )
+    cur = conn.cursor()
+    for c,l in url_map.items():
+        db_name = c + '_poi_data'
+        print(f'Running {c} loop...')
+        cur.execute(f'DROP TABLE IF EXISTS {db_name}')
+        cur.execute(f'CREATE TABLE IF NOT EXISTS {db_name} (county TEXT, topic Text, num_of_features NUMBER)')
+        location = str(c)
+        for point in poi:
+            url = 'https://api.geoapify.com/v2/places?categories=' + point + '&filter=place:' + l + '&limit=100&apiKey=' + api_key
+            response = requests.get(url)
+            data = response.json()
+            feature_length = len(data['features'])
+            cur.execute(f"INSERT INTO {db_name} VALUES ('{location}','{point}', '{feature_length}')")
     
+        print(f'{c} loop done')
+    
+    print('POI data done')
+
+
+ 
 
 def get_community_data():
     # getting neighborhood,
@@ -136,11 +177,10 @@ def get_community_data():
 
 
 """
-1 stage the data
-2 copy into table with format JSON (raw_data) 
-3 use a create table as select statement to create a table from flattened (flattened_data) in order to keep the JSON format for changes and bug searching
-3a drop raw_data table
-4 Create final table for parks a'la the below
+Inside get_poi_data() I need to do the following:
+1 search for each location for the important poi's
+    NOTE: see if I can search multiple categories like restaurants and parks and groceries etc etc
+2 Follow the method from get_community_data() to get and upload all JSON content to raw DB's
 
 
 Below: src is the column of JSON data in raw script
